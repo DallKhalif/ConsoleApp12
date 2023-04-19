093e2a24d5b847c0a7ef33e79e79013349a82a10
DallKhalif <116428855+DallKhalif@users.noreply.github.com>
2023-04-19 15:48:52 +0430

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Compaer();
+
+        }
+        static void Compaer()
+        {
+            try
+            {
+                Console.WriteLine("Enter the value a : ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the value b : ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                if (a > b)
+                {
+                    Console.WriteLine(true + "\n" + "a is bigger than b");
+                }
+                else
+                {
+                    Console.WriteLine(false + "\n" + "a is less or equal to b");
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine(false + "\n" + "Just enter the intejer value ");
+            }
+        }
+    }
+}
