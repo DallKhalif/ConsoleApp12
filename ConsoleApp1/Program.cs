415e6c0dad7598e083f710b61273e14f20583d7e
Mohammad Jahanbakhsh <116428855+DallKhalif@users.noreply.github.com>
2023-04-29 09:11:09 +0330

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Compaer("saa","saa");
+        }
+        public static void Compaer(string a, string b)
+        {
+            if (string.Compare(a,b) >= 0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
