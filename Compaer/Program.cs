e9e9a9ae26f4025efd34e85eb10da55a6f1caea1
Mohammad Jahanbakhsh <116428855+DallKhalif@users.noreply.github.com>
2023-04-29 14:01:24 +0330

    Add files via upload

---
+++ Compaer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Compaer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           bool myCompaer =  Compaers(Console.ReadLine(), Console.ReadLine());
+           Console.WriteLine(myCompaer);
+        }
+        static bool Compaers(int a, int b)
+        {
+            if (a >= b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool Compaers(string a, string b)
+        {
+            if (string.Compare(a, b) >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool Compaers(double a, double b)
+        {
+            if (a >= b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool Compaers(char a, char b)
+        {
+            int c = a.CompareTo(b);
+            if (c >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
