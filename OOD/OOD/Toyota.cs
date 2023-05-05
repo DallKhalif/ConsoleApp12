1de0ed395bf514c852e703353f99f338a070d25d
Mohammad Jahanbakhsh <116428855+DallKhalif@users.noreply.github.com>
2023-05-05 14:09:41 +0330

    Add files via upload

---
+++ OOD/OOD/Toyota.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Toyota : ICars
+    {
+        public void bodyType()
+        {
+            Console.WriteLine("Chose the body type : ");
+            Console.ReadLine();
+        }
+
+        public void color()
+        {
+            Console.WriteLine("Chose the color : ");
+            Console.ReadLine();
+        }
+        public void engine()
+        {
+            Console.WriteLine("Chose the engine : ");
+            Console.ReadLine();
+        }
+
+        public void model()
+        {
+            Console.WriteLine("Chose the model : ");
+            Console.ReadLine();
+        }
+
+        public void option()
+        {
+            Console.WriteLine("Chose the option : ");
+            Console.ReadLine();
+        }
+        public void year()
+        {
+            Console.WriteLine("Chose the color : ");
+            Console.ReadLine();
+        }
+    }
+}
